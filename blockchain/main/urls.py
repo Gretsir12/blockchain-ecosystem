054b9a863c953823693eb67a41e1fff757e4bd7e
Gretsir12 <9219949422iv@gmail.com>
2025-04-29 11:11:48 +0300

    Set render of main and about pages

--- blockchain/main/urls.py
+++ blockchain/main/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
-from .views import index
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', views.index),
+    path('about', views.about)
 ]
