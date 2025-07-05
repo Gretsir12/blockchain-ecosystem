46541fd7beec8dcadf9b795fce9156c1093211ee
Gretsir12 <9219949422iv@gmail.com>
2025-07-05 13:22:37 +0300

    work with form

--- blockchain/main/views.py
+++ blockchain/main/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+from .forms import UserForm
 # from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'main/index.html')
+    def index(request):
+        userform = UserForm()
+        return render(request, "main/index.html",
+        { "form": userform})
 
 def about(request):
     return render(request, 'main/about.html')
