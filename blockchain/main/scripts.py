5e617f5d8f4ce74015709ee9466e89ef783d02ea
Gretsir12 <9219949422iv@gmail.com>
2025-07-10 13:12:46 +0300

    remove unused registration validation script and database; update index.html to link external CSS

---
+++ blockchain/main/scripts.py
@@ -0,0 +1,36 @@
+import sqlite3
+# import re
+
+# db = sqlite3.connect('users.db') #creating database
+
+def send_mail(FROM,TO,SUBJECT,TEXT,SERVER):
+    import smtplib
+    message = """\
+    From: %s
+    To: %s
+    Subject: %s
+    %s
+    """ % (FROM, ",".join(TO), SUBJECT, TEXT)
+    # Send the mail
+    SERVER = smtplib.SMTP(SERVER)
+    SERVER.sendmail(FROM, TO, message)
+    SERVER.quit()
+
+def registration(username, email, password, db):
+    try:
+        # Connect to SQLite Database and create a cursor
+        sqliteConnection = sqlite3.connect('databases/users.db')
+        cursor = sqliteConnection.cursor()
+        print('DEBUG: Connect to users.db successfully')
+        # Fetch and print the result
+        cursor = sqlite3.createCursor()
+        cursor.execute
+        print('SQLite Version is {}'.format(result[0][0]))
+        # Close the cursor after use
+        cursor.close()
+
+    except sqlite3.Error as error:
+        print('DEBUG: Error occurred -', error)
+ # creating cursor
+
+    db.close()
