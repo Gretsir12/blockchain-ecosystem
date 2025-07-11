527c1dc8c2675681229c3551635104e0df962461
Vlad Afonskiy <146872011+alohadance48@users.noreply.github.com>
2025-07-11 23:14:41 +0300

    Update models.py

--- blockchain/main/models.py
+++ blockchain/main/models.py
@@ -2,15 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class User(models.Model):
-    username = models.CharField(max_length=100, unique=True)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=255)  # Store hashed passwords in production
+class User(AbstractUser)):
 
     def __str__(self):
         return self.username
-
-    class Meta:
-        verbose_name = 'User'
-        verbose_name_plural = 'Users'
-        ordering = ['username']  # Order by username by default
